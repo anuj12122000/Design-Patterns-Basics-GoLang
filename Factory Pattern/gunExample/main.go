8736aed4ea7090c0ff18d6b546edca18f3171a52
anuj12122000 <62623809+anuj12122000@users.noreply.github.com>
2023-11-27 17:34:29 +0530

    added the Change

--- Factory Pattern/gunExample/main.go
+++ Factory Pattern/gunExample/main.go
@@ -24,8 +24,13 @@ type Ak47 struct {
 	Gun // here we embed / inject the Gun struct in the ak47 struct
 }
 
+func (ak Ak47) GetName() string { //best Example of Loosely Coupled Environment Using the Factory Pattern
+	fmt.Println("DO SOMETHING ELSE THAN NORMAL ")
+	return "NORMAL AK47 Context"
+}
+
 func newAk47() IGun {
-	return &Ak47{
+	return &Ak47{ // always remember this piece of code , rat lo ya samjh lo , ek he baat hain
 		Gun: Gun{
 			name:  "ak47",
 			power: "2",
@@ -55,5 +60,5 @@ func getFactory(id int) IGun {
 
 func main() {
 	firstType := getFactory(1)
-	fmt.Println(firstType)
+	fmt.Println(firstType.GetName())
 }
